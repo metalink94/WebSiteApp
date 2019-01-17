62166afbc640018ad6c94d66677d95bd012f2367
Denis <denisnovikov94@gmail.com>
2019-01-17 21:44:34 +0300

    add 4rabbet flavers

---
+++ app/src/main/java/ru/lopav/kzn/websiteapp/SplashScreen.kt
@@ -0,0 +1,22 @@
+package ru.lopav.kzn.websiteapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+
+class SplashScreen: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            showMainScreen()
+        }, 1500)
+    }
+
+    private fun showMainScreen() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
