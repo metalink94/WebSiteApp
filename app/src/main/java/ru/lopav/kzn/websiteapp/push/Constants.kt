4239354f0ead4da91757593761b13dcc2dd10d87
Денис <denisnovikov94@gmail.com>
2019-04-08 12:11:13 +0300

    add trading

--- app/src/main/java/ru/lopav/kzn/websiteapp/push/Constants.kt
+++ app/src/main/java/ru/lopav/kzn/websiteapp/push/Constants.kt
@@ -6,8 +6,8 @@ object Constants {
     val DATABASE_IS_FINISH = "isFinish"
     val DATABASE_URL = "url"
 
-    val FIRST_APP = "com.united.push.j9cs7"
-    val SECOND_APP = "com.united.push.wqs6cs"
+    val FIRST_APP = "com.united.push.dZpRRg"
+    val SECOND_APP = "com.united.push.CK1pd"
     val DATABASE_URL_2 = "urlSecond"
     val DATABASE_URL_3 = "urlThird"
 }
