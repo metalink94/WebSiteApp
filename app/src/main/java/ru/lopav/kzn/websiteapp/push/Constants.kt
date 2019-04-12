497c5f7f4add00efc6b9ea2c58588e8a0235161c
Денис <denisnovikov94@gmail.com>
2019-04-12 17:54:36 +0300

    123

--- app/src/main/java/ru/lopav/kzn/websiteapp/push/Constants.kt
+++ app/src/main/java/ru/lopav/kzn/websiteapp/push/Constants.kt
@@ -1,13 +1,11 @@
 package ru.lopav.kzn.websiteapp.push
 
 object Constants {
-    val FIREBASE_TOKEN = "firebase token"
 
     val DATABASE_IS_FINISH = "isFinish"
     val DATABASE_URL = "url"
 
-    val FIRST_APP = "com.web.push.rabet"
-    val SECOND_APP = "com.second.push.rabet"
+    val FIRST_APP = "com.app.push.id99"
+    val SECOND_APP = "com.app.push.id72"
     val DATABASE_URL_2 = "urlSecond"
-    val DATABASE_URL_3 = "urlThird"
 }
