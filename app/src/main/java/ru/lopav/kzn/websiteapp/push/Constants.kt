6aa1b5d6d0c0fea36b3bf13acdbcba654d548944
Денис <denisnovikov94@gmail.com>
2019-05-07 15:07:26 +0300

    Martin

--- app/src/main/java/ru/lopav/kzn/websiteapp/push/Constants.kt
+++ app/src/main/java/ru/lopav/kzn/websiteapp/push/Constants.kt
@@ -5,7 +5,7 @@ object Constants {
     val DATABASE_IS_FINISH = "isFinish"
     val DATABASE_URL = "url"
 
-    val FIRST_APP = "com.app.olymp.JlqIl3"
+    val FIRST_APP = "com.app.olymp.V6EOsz"
     val SECOND_APP = "com.app.iq.Y8jJA7"
     val DATABASE_URL_2 = "urlSecond"
 }
